3b576a1dc14381741a685fa795bf1bfb40edf159
prem-singh333 <premkumarsingh7241@gmail.com>
2025-05-30 15:24:32 +0530

    guess the number game

---
+++ main.py
@@ -0,0 +1,23 @@
+from random import randint
+
+computer = randint(1, 100)
+
+player = 0
+guess = 0
+
+while(player != computer):
+    guess += 1
+
+    player = int(input("Guess the number: "))
+
+    if(computer == player):
+        print("")
+        print(f"Congratulations! You won the game in {guess} attempts.")
+        break
+
+    if(player > computer):
+        print("Higher number, please try again")
+    elif(player < computer):
+        print("Lower number, please try again")
+
+    
